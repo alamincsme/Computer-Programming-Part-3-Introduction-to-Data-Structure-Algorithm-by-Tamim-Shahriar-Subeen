3decb99d39f74d59eec9a11c32afcb776c668e6e
Al Amine <alamincsme@gmail.comm>
2023-10-13 21:40:48 +0600

    Time and Space Complexity

--- Chap01 - Time & Space Complexity/Solution_02.java
+++ Chap01 - Time & Space Complexity/Solution_02.java
@@ -3,6 +3,7 @@ package data_structure_and_algorithm;
 import java.util.Scanner;
 
 public class Solution_02 {
+    
     private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
