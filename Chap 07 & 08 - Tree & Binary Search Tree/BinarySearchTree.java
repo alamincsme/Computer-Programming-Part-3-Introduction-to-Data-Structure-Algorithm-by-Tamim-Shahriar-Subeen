567d10db2201d23493252db0104cf0ec702bd2cc
Al Amine <alamincsme@gmail.comm>
2023-11-14 05:46:14 +0600

    Binary Search Tree using Loop

---
+++ Chap 07 & 08 - Tree & Binary Search Tree/BinarySearchTree.java
@@ -0,0 +1,80 @@
+package practice;
+
+
+class Node {
+    int data;
+    Node left , right;
+
+    public Node (int item) {
+        this.data = item;
+        this.left = this.right = null;
+    }
+}
+
+public class BinarySearchTree {
+     Node root ;
+
+     public BinarySearchTree() {
+         root = null;
+     }
+
+     public void insert(int item) {
+         root = inserRec(root , item);
+
+     }
+
+    private Node inserRec(Node root, int item) {
+         if (root == null) {
+             return new Node(item);
+         }
+
+         Node parent_node, current_node;
+         parent_node = null;
+         current_node = root;
+
+         while (current_node != null) {
+             parent_node = current_node;
+
+             if (item < current_node.data) {
+                 current_node = current_node.left;
+             } else {
+                 current_node = current_node.right;
+             }
+         }
+
+         if (item < parent_node.data) {
+             parent_node.left = new Node(item);
+         } else {
+             parent_node.right = new Node(item);
+         }
+
+         return root ;
+    }
+
+    public void inOrder() {
+         inOrderRec(root);
+    }
+
+    private void inOrderRec(Node root) {
+         if (root != null) {
+             inOrderRec(root.left);
+             System.out.println(root.data + " ");
+             inOrderRec(root.right);
+         }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree BST = new BinarySearchTree();
+
+        int [] in = { 2, 7 , 1};
+
+        for (int item : in) {
+            BST.insert(item);
+        }
+
+        System.out.println("inorder traversal :");
+        BST.inOrder();
+    }
+
+
+}
